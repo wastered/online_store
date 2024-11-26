303b10b54ff36b68ccf3406014b7af720842aaf4
wastered <nonamelite@mail.ru>
2024-11-26 18:22:44 +0300

    Добавлен экспорт Order в CSV-файл

--- server/apps/orders/admin.py
+++ server/apps/orders/admin.py
@@ -1,6 +1,39 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
+from django.utils.datetime_safe import datetime
 
 from .models import Order, OrderItem
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f"attachment; filename={opts.verbose_name.lower()}.csv"
+    response = HttpResponse(content_type="text/csv")
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+
+    # записать первую строку с информацией заголовка
+    writer.writerow([field.verbose_name for field in fields])
+
+    # записать строки данных
+    for obj in queryset:
+        data_row = []
+
+        for field in fields:
+            value = getattr(obj, field.name)
+
+            if isinstance(value, datetime):
+                value = value.strftime("%d/%m/%Y")
+
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -15,3 +48,4 @@ class OrderAdmin(admin.ModelAdmin):
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
