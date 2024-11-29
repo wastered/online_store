ce9ee8db09aabb775fe34f6ec12e1b9ef62b4336
wastered <nonamelite@mail.ru>
2024-11-29 12:21:17 +0300

    Добавлена страница отображения детальной инфы по заказу

--- server/apps/orders/admin.py
+++ server/apps/orders/admin.py
@@ -2,7 +2,9 @@ import csv
 
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
 from django.utils.datetime_safe import datetime
+from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
 
@@ -41,11 +43,16 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
--- server/apps/orders/views.py
+++ server/apps/orders/views.py
@@ -1,8 +1,9 @@
 from django.urls import reverse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
 
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 from .tasks import order_created
 from ..cart.cart import Cart
 
@@ -38,4 +39,13 @@ def order_create(request):
                   context={
                       'cart': cart,
                       'form': form,
+                  })
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html',
+                  context={
+                      'order': order,
                   })
